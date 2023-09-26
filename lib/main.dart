75ea0029add2b542ce7cb2b2197f04b22e5d8729
kangjl <kangjinling99@gmail.com>
2023-09-26 17:42:11 +0800

    提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import './pickers/hsv_picker.dart';
-import './pickers/material_picker.dart';
-import './pickers/block_picker.dart';
+import 'package:led_light/pickers/hue_picker.dart';
+import 'package:led_light/pickers/utils.dart';
 
-void main() => runApp(const MaterialApp(home: MyApp()));
+void main() => runApp(
+    const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyApp()));
 
 class MyApp extends StatefulWidget {
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -15,7 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool lightTheme = true;
-  Color currentColor = Colors.amber;
+  Color currentColor = Colors.blueAccent;
   List<Color> currentColors = [Colors.yellow, Colors.green];
   List<Color> colorHistory = [];
 
@@ -24,55 +26,23 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
     final foregroundColor = useWhiteForeground(currentColor) ? Colors.white : Colors.black;
     return AnimatedTheme(
       data: lightTheme ? ThemeData.light() : ThemeData.dark(),
-      child: Builder(builder: (context) {
-        return DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            floatingActionButton: FloatingActionButton.extended(
-              onPressed: () => setState(() => lightTheme = !lightTheme),
-              icon: Icon(lightTheme ? Icons.dark_mode_rounded : Icons.light_mode_rounded),
-              label: Text(lightTheme ? 'Night' : '  Day '),
-              backgroundColor: currentColor,
-              foregroundColor: foregroundColor,
-              elevation: 15,
-            ),
-            appBar: AppBar(
-              title: const Text('Flutter Color Picker Example'),
-              backgroundColor: currentColor,
-              foregroundColor: foregroundColor,
-              bottom: TabBar(
-                labelColor: foregroundColor,
-                tabs: const <Widget>[
-                  Tab(text: 'HSV/HSL/RGB'),
-                  Tab(text: 'Material'),
-                  Tab(text: 'Blocky'),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: <Widget>[
-                HSVColorPickerExample(
-                  pickerColor: currentColor,
-                  onColorChanged: changeColor,
-                  colorHistory: colorHistory,
-                  onHistoryChanged: (List<Color> colors) => colorHistory = colors,
-                ),
-                MaterialColorPickerExample(pickerColor: currentColor, onColorChanged: changeColor),
-                BlockColorPickerExample(
-                  pickerColor: currentColor,
-                  onColorChanged: changeColor,
-                  pickerColors: currentColors,
-                  onColorsChanged: changeColors,
-                  colorHistory: colorHistory,
-                ),
-              ],
-            ),
-          ),
-        );
-      }),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('流光灯',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),
+          backgroundColor: currentColor,
+          foregroundColor: foregroundColor,
+        ),
+        body: ColorPickerExample(
+          pickerColor: currentColor,
+          onColorChanged: changeColor,
+          colorHistory: colorHistory,
+          onHistoryChanged: (List<Color> colors) => colorHistory = colors,
+        ),
+      ),
     );
   }
 }
---
+++ lib/pickers/hue_picker.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart' show CupertinoTextField;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:led_light/pickers/palette.dart';
+import 'package:led_light/pickers/utils.dart';
+
+import 'colorpicker.dart';
+
+// Just an example of how to use/interpret/format text input's result.
+void copyToClipboard(String input) {
+  String textToCopy = input.replaceFirst('#', '').toUpperCase();
+  if (textToCopy.startsWith('FF') && textToCopy.length == 8) {
+    textToCopy = textToCopy.replaceFirst('FF', '');
+  }
+  Clipboard.setData(ClipboardData(text: '#$textToCopy'));
+}
+
+class ColorPickerExample extends StatefulWidget {
+  const ColorPickerExample({
+    Key? key,
+    required this.pickerColor,
+    required this.onColorChanged,
+    this.colorHistory,
+    this.onHistoryChanged,
+  }) : super(key: key);
+
+  final Color pickerColor;
+  final ValueChanged<Color> onColorChanged;
+  final List<Color>? colorHistory;
+  final ValueChanged<List<Color>>? onHistoryChanged;
+
+  @override
+  State<ColorPickerExample> createState() => _ColorPickerExample();
+}
+
+class _ColorPickerExample extends State<ColorPickerExample> {
+
+
+  // Picker 4
+  final textController =
+      TextEditingController(text: '#2F19DB'); // The initial value can be provided directly to the controller.
+  bool _enableAlpha4 = true;
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ColorPicker(
+          pickerColor: widget.pickerColor,
+          onColorChanged: widget.onColorChanged,
+          colorPickerWidth: 400,
+          pickerAreaHeightPercent: 0.8,
+          enableAlpha: false, // hexInputController will respect it too.
+          displayThumbColor: true,
+          paletteType: PaletteType.hueWheel,
+          labelTypes: const [],
+          pickerAreaBorderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(2),
+            topRight: Radius.circular(2),
+          ),
+          hexInputController: textController, // <- here
+          portraitOnly: true,
+          hexInputBar: false,
+          colorHistory: widget.colorHistory,
+          onHistoryChanged: widget.onHistoryChanged,
+        ),
+        Text(textController.text),
+      ],
+    );
+  }
+}
